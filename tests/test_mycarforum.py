51cd98a6063dd009da0abf850dd85e546a9984ee
lunaflight <e0958283@u.nus.edu>
2025-11-01 17:00:29 +0800

    chore: split tests into different files

---
+++ tests/test_mycarforum.py
@@ -0,0 +1,43 @@
+from tests import url_checker
+
+
+def test_mycarforum_reply() -> None:
+    test_suffix = "reply"
+    url = "https://www.mycarforum.com/forums/topic/2111034-bringbuy-beer-from-sg-to-bintan-resorts/?do=findComment&comment=2115341"
+    expected_post = "yeah lor.. really hope so...<br>but mebbe not doing sea sports..<br>more likely spa etc..<br>hahaa<br>Wah, good way to relax!<br>C u there!!<br>Aiya!! I guess MCF should start selling member's T-shirt to support the forum, then all those going Bintan can hi to each other."  # noqa: E501
+    expected_credit = (
+        "2007 Nov 5, Davidcks. Mycarforum, "
+        f'"Bring/Buy beer from SG to Bintan resorts?". {url}'
+    )
+    url_checker.assert_produces_post_and_credit(
+        url=url,
+        test_suffix=test_suffix,
+        expected_post=expected_post,
+        expected_credit=expected_credit,
+    )
+
+
+def test_mycarforum_main_post() -> None:
+    test_suffix = "topic"
+    url = "https://www.mycarforum.com/forums/topic/2111034-bringbuy-beer-from-sg-to-bintan-resorts/"
+    expected_post = (
+        "Bring/Buy beer from SG to Bintan resorts?<br>Hi, I need advise "
+        "from the experts who visit bintan resorts often. Me with some "
+        "family and  friends are going over to bintan lagoon next long "
+        "weekend.<br>And was told that beer are expensive there, so was "
+        "wondering if it is a good idea to buy in SG and bring it across, "
+        "or is there any duly free shops at TM ferry terminal to purchase "
+        "from?? Someone told me that it is ok to bring a six pack each "
+        "person into bintan, is this true?<br>Does anyone has any "
+        "suggestion or advise?? 10q 10q."
+    )
+    expected_credit = (
+        '2007 Nov 1, Davidcks. Mycarforum, "Bring/Buy beer from SG to '
+        f'Bintan resorts?". {url}'
+    )
+    url_checker.assert_produces_post_and_credit(
+        url=url,
+        test_suffix=test_suffix,
+        expected_post=expected_post,
+        expected_credit=expected_credit,
+    )
---
+++ tests/test_soft.py
@@ -0,0 +1,17 @@
+from tests import url_checker
+
+
+def test_soft_reply() -> None:
+    test_suffix = "reply"
+    url = "https://soft.com.sg/threads/childhood-jeers.6640/#post-108928"
+    expected_post = 'haha...suddenly remembered more...<br>Copy cat, kiss the rat, go home let your mother slap, father say, "nevermind". Mother say, "Go and die!"<br>i learnt that in kindergarten, then when i recited it at home, my mum was appalled, and told me not to chant it anymore...lol!'  # noqa: E501
+    expected_credit = (
+        f'2005 Oct 30, MichaelAngelo. S.O.F.T., "Childhood jeers". {url}'
+    )
+
+    url_checker.assert_produces_post_and_credit(
+        url=url,
+        test_suffix=test_suffix,
+        expected_post=expected_post,
+        expected_credit=expected_credit,
+    )
