02e272b270e623106df067ecc91499c12a85baac
lunaflight <e0958283@u.nus.edu>
2025-11-06 00:07:22 +0800

    feat: expand ellipses unicode to ASCII

--- tests/test_hardwarezone.py
+++ tests/test_hardwarezone.py
@@ -39,3 +39,23 @@ def test_main_post() -> None:
         expected_post=expected_post,
         expected_credit=expected_credit,
     )
+
+
+def test_replace_ellipses_with_ascii() -> None:
+    test_suffix_for_caching = "ellipses"
+    url = "https://forums.hardwarezone.com.sg/threads/sg-soon-no-need-water-from-malaysia-liao.7105036/post-155324874"
+    expected_post = "If we have sufficient solar electricity or nuclear power generator... we can have endless supply of water...<br>We are limited by energy..."  # noqa: E501
+    expected_credit = (
+        "2025 Feb 15, "
+        "Can Or Not. "
+        "HardwareZone, "
+        '"SG soon no need water from malaysia liao". '
+        f"{url}"
+    )
+
+    url_checker.assert_produces_post_and_credit(
+        url=url,
+        test_suffix_for_caching=test_suffix_for_caching,
+        expected_post=expected_post,
+        expected_credit=expected_credit,
+    )
