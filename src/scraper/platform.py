93252d5164ae15bcccd65c9bd147927b6096dcd2
lunaflight <e0958283@u.nus.edu>
2025-11-12 00:52:21 +0800

    chore: rename field of platform to SiteName
    This resolves the TODO, since it was confusing for platform.py to also
    contain a field called platform.

--- src/scraper/platform.py
+++ src/scraper/platform.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from src.scraper import assertation, invision, reddit, xenforo
 
 
-class Platform(Enum):
+class SiteName(Enum):
     BLOWINGWIND = "Blowing_wind"
     HARDWAREZONE = "HardwareZone"
     MYCARFORUM = "Mycarforum"
@@ -16,107 +16,105 @@ class Platform(Enum):
     SOFT = "S.O.F.T."
 
 
-# TODO: This file could use a better name, since platform is already one
-# of its fields.
 @dataclass
 class T:
-    platform: Platform
+    site_name: SiteName
     url: str
 
 
 def to_stylised_string(t: T) -> str:  # noqa: PLR0911
-    match t.platform:
-        case Platform.BLOWINGWIND:
+    match t.site_name:
+        case SiteName.BLOWINGWIND:
             return "Blowing Wind"
-        case Platform.HARDWAREZONE:
+        case SiteName.HARDWAREZONE:
             return "HardwareZone"
-        case Platform.MYCARFORUM:
+        case SiteName.MYCARFORUM:
             return "Mycarforum"
-        case Platform.REDDIT:
+        case SiteName.REDDIT:
             return "Reddit"
-        case Platform.SINGAPOREBRIDES:
+        case SiteName.SINGAPOREBRIDES:
             return "SingaporeBrides"
-        case Platform.SINGAPOREMOTHERHOOD:
+        case SiteName.SINGAPOREMOTHERHOOD:
             return "SingaporeMotherhood"
-        case Platform.SOFT:
+        case SiteName.SOFT:
             return "S.O.F.T."
 
-    raise ValueError("Unknown platform.")
+    raise ValueError("Unknown site_name.")
 
 
 def to_plain_string(t: T) -> str:
     return to_stylised_string(t).lower().replace(" ", "_")
 
 
-def identifying_url_substring(platform: Platform) -> str:  # noqa: PLR0911
-    match platform:
-        case Platform.BLOWINGWIND:
+def identifying_url_substring(site_name: SiteName) -> str:  # noqa: PLR0911
+    match site_name:
+        case SiteName.BLOWINGWIND:
             return "blowingwind.io"
-        case Platform.HARDWAREZONE:
+        case SiteName.HARDWAREZONE:
             return "hardwarezone.com.sg"
-        case Platform.MYCARFORUM:
+        case SiteName.MYCARFORUM:
             return "mycarforum.com"
-        case Platform.REDDIT:
+        case SiteName.REDDIT:
             return "reddit.com"
-        case Platform.SINGAPOREBRIDES:
+        case SiteName.SINGAPOREBRIDES:
             return "singaporebrides.com"
-        case Platform.SINGAPOREMOTHERHOOD:
+        case SiteName.SINGAPOREMOTHERHOOD:
             return "singaporemotherhood.com"
-        case Platform.SOFT:
+        case SiteName.SOFT:
             return "soft.com.sg"
 
-    raise ValueError("Unknown platform.")
+    raise ValueError("Unknown site_name.")
 
 
 def of_url(url: str) -> T:
-    for platform_ in Platform:
-        if identifying_url_substring(platform_) in url:
-            return T(platform=platform_, url=url)
-    raise ValueError("The URL does not match any known platform.")
+    for site_name_ in SiteName:
+        if identifying_url_substring(site_name_) in url:
+            return T(site_name=site_name_, url=url)
+    raise ValueError("The URL does not match any known site_name.")
 
 
 def get_assertation(t: T) -> assertation.T:
-    match t.platform:
-        case Platform.BLOWINGWIND | Platform.MYCARFORUM:
+    match t.site_name:
+        case SiteName.BLOWINGWIND | SiteName.MYCARFORUM:
             return invision.of_url(t.url).assertation()
-        case Platform.REDDIT:
+        case SiteName.REDDIT:
             return reddit.of_url(t.url).assertation()
         case (
-            Platform.HARDWAREZONE
-            | Platform.SINGAPOREBRIDES
-            | Platform.SINGAPOREMOTHERHOOD
-            | Platform.SOFT
+            SiteName.HARDWAREZONE
+            | SiteName.SINGAPOREBRIDES
+            | SiteName.SINGAPOREMOTHERHOOD
+            | SiteName.SOFT
         ):
             return xenforo.of_url(t.url).assertation()
 
 
 def get_soup_for_testing(t: T) -> BeautifulSoup:
-    match t.platform:
-        case Platform.BLOWINGWIND | Platform.MYCARFORUM:
+    match t.site_name:
+        case SiteName.BLOWINGWIND | SiteName.MYCARFORUM:
             return invision.of_url(t.url).soup
-        case Platform.REDDIT:
+        case SiteName.REDDIT:
             return reddit.of_url(t.url).soup_from_old
         case (
-            Platform.HARDWAREZONE
-            | Platform.SINGAPOREBRIDES
-            | Platform.SINGAPOREMOTHERHOOD
-            | Platform.SOFT
+            SiteName.HARDWAREZONE
+            | SiteName.SINGAPOREBRIDES
+            | SiteName.SINGAPOREMOTHERHOOD
+            | SiteName.SOFT
         ):
             return xenforo.of_url(t.url).soup
 
 
 def get_assertation_for_testing(t: T, soup: BeautifulSoup) -> assertation.T:
-    match t.platform:
-        case Platform.BLOWINGWIND | Platform.MYCARFORUM:
+    match t.site_name:
+        case SiteName.BLOWINGWIND | SiteName.MYCARFORUM:
             return invision.of_url_with_soup(url=t.url, soup=soup).assertation()
-        case Platform.REDDIT:
+        case SiteName.REDDIT:
             return reddit.of_url_with_soup(
                 url=t.url, soup_from_old=soup
             ).assertation()
         case (
-            Platform.HARDWAREZONE
-            | Platform.SINGAPOREBRIDES
-            | Platform.SINGAPOREMOTHERHOOD
-            | Platform.SOFT
+            SiteName.HARDWAREZONE
+            | SiteName.SINGAPOREBRIDES
+            | SiteName.SINGAPOREMOTHERHOOD
+            | SiteName.SOFT
         ):
             return xenforo.of_url_with_soup(url=t.url, soup=soup).assertation()
