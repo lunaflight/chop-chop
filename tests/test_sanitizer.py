af94d35dace79c160d4670601ea8b84c05999884
lunaflight <e0958283@u.nus.edu>
2025-11-15 02:58:45 +0800

    chore: use expecttest for test_sanitizer.py

--- tests/test_sanitizer.py
+++ tests/test_sanitizer.py
@@ -1,17 +1,20 @@
+from expecttest import assert_expected_inline
+
 from src import sanitizer
 
 
 def test_ufeff_stripped() -> None:
     string = " \ufeffhttps://en.wiktionary.org/wiki/%E9%8D%8B "
-    expected_string = "https://en.wiktionary.org/wiki/%E9%8D%8B"
 
-    assert (
-        sanitizer.clean_input_for_utf8_compatibility(string) == expected_string
+    assert_expected_inline(
+        sanitizer.clean_input_for_utf8_compatibility(string),
+        """https://en.wiktionary.org/wiki/%E9%8D%8B""",
     )
 
 
 def test_clean_unicode_for_output() -> None:
     string = "bad\xa0 characters…"
-    expected_string = "bad characters..."
 
-    assert sanitizer.clean_unicode_for_output(string) == expected_string
+    assert_expected_inline(
+        sanitizer.clean_unicode_for_output(string), """bad characters..."""
+    )
