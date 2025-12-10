54935110d0be6c1c2e4a8102d44d4096a9c281ff
lunaflight <e0958283@u.nus.edu>
2025-12-10 18:04:06 +0800

    test(linter): demonstrate redirected linked words detection

--- tests/linter/commons.py
+++ tests/linter/commons.py
@@ -1,2 +1,6 @@
 IRRELEVANT = "empty for testing"
 FIXED_WORD = "word"
+
+
+def get_fixed_word(seed: int) -> str:
+    return f"{FIXED_WORD}_{seed}"
--- tests/linter/rules/test_linked_words_are_known.py
+++ tests/linter/rules/test_linked_words_are_known.py
@@ -5,7 +5,9 @@ from expecttest import assert_expected_inline
 from src.linter import rule_result
 from src.linter.json import entry
 from src.linter.rules import linked_words_are_known
-from tests.linter.commons import FIXED_WORD, IRRELEVANT
+from tests.linter.commons import FIXED_WORD, IRRELEVANT, get_fixed_word
+
+ANOTHER_FIXED_WORD = get_fixed_word(seed=1)
 
 
 def lint_and_get_result(
@@ -67,4 +69,21 @@ def test_quoted() -> None:
     assert_expected_inline(
         lint_and_get_result(quoted_word=FIXED_WORD, known_words=[]),
         """ERROR: Found "word", which does not link to a valid word.""",
+    )
+
+
+def test_redirected_quote() -> None:
+    assert_expected_inline(
+        lint_and_get_result(
+            quoted_word=f"{ANOTHER_FIXED_WORD}|{FIXED_WORD}",
+            known_words=[ANOTHER_FIXED_WORD],
+        ),
+        """ERROR: Found "word", which does not link to a valid word.""",
+    )
+    assert_expected_inline(
+        lint_and_get_result(
+            quoted_word=f"{ANOTHER_FIXED_WORD}|{FIXED_WORD}",
+            known_words=[FIXED_WORD],
+        ),
+        """OK""",
     )
