2964eb9c0537d40fab43040126ad6d413d7bfc19
lunaflight <e0958283@u.nus.edu>
2026-02-02 05:58:54 +0700

    chore(linter): use find_duplicate

--- src/linter/rules/no_repeat_definitions.py
+++ src/linter/rules/no_repeat_definitions.py
@@ -1,5 +1,6 @@
 from src.linter import rule_result
 from src.linter.json import entry
+from src.python_lib_functions import find_duplicate
 
 
 def description() -> str:
@@ -8,16 +9,14 @@ def description() -> str:
 
 def lint(entry_: entry.T) -> rule_result.T:
     for part_of_speech in entry_.meanings.values():
-        seen_definitions: set[str] = set()
+        definitions = [
+            definition_entry.definition for definition_entry in part_of_speech
+        ]
 
-        for definition_entry in part_of_speech:
-            definition = definition_entry.definition
-
-            if definition in seen_definitions:
-                return rule_result.error(
-                    f"Found repeat definition: {definition}"
-                )
-
-            seen_definitions.add(definition)
+        duplicate_definition = find_duplicate(definitions)
+        if duplicate_definition:
+            return rule_result.error(
+                f"Found repeat definition: {duplicate_definition}"
+            )
 
     return rule_result.ok()
